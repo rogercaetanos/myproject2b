f585a41e17904c28cca03a13c85234e510b62278
Rogério Caetano <rogercaetanos@gmail.com>
2023-08-25 12:03:25 -0300

    Criação do ambiente de desenvolvimento

---
+++ src/main/java/com/itb/tcc/myproject2b/Myproject2bApplication.java
@@ -0,0 +1,13 @@
+package com.itb.tcc.myproject2b;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Myproject2bApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Myproject2bApplication.class, args);
+	}
+
+}
